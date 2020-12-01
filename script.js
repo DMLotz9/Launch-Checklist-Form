650b8c28af07f853c81f840dc09a8bbfa5086604
Dylan Lotz <dschnake09@yahoo.com>
2020-11-30 19:22:34 -0600

    added final requirements and refactored script.js

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
 window.onload=function(){
-let form = document.querySelector("form");
-form.addEventListener("submit", function(){
    fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
       response.json().then(function(json){
       document.getElementById("missionTarget").innerHTML= `      <h2>Mission Destination</h2>
       <ol>
-         <li>Name: ${json[0].name}</li>
-         <li>Diameter: ${json[0].diameter}</li>
-         <li>Star: ${json[0].star}</li>
-         <li>Distance from Earth: ${json[0].distance}</li>
-         <li>Number of Moons: ${json[0].moons}</li>
+         <li>Name: ${json[2].name}</li>
+         <li>Diameter: ${json[2].diameter}</li>
+         <li>Star: ${json[2].star}</li>
+         <li>Distance from Earth: ${json[2].distance}</li>
+         <li>Number of Moons: ${json[2].moons}</li>
       </ol>
-      <img src="${json[0].image}"></img>`;
-   }); 
-});
-
-});
-}
+      <img src="${json[2].image}"></img>`;
+      }); 
+   });
+};
